56e0ca9c3575d1d79983423d8322686d3b3d3f30
tomkim825 <tk825@hotmail.com>
2018-09-19 22:30:40 -0700

    adding routing

--- src/index.js
+++ src/index.js
@@ -6,19 +6,18 @@ import Personal from './Personal';
 import registerServiceWorker from './registerServiceWorker';
 import { BrowserRouter,Switch, Route } from 'react-router-dom';
 
-// const Main = () => (
-//     <main>
-//       <Switch>
-//         <Route exact path='/' component={App}/>
-//         <Route path='/:symbol' component={Personal}/>
-//       </Switch>
-//     </main>
-//   )
+const Main = () => (
+    <main>
+      <Switch>
+        <Route exact path='/' component={App}/>
+        <Route path='/:symbol' component={Personal}/>
+      </Switch>
+    </main>
+  )
 
 ReactDOM.render(  
-  <App/>
-// {/* <BrowserRouter>
-//     <Main/>
-//   </BrowserRouter> */}
+<BrowserRouter>
+    <Main/>
+  </BrowserRouter> 
   , document.getElementById('root'));
 registerServiceWorker();
